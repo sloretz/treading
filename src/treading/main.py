0e263b09e27f2d4b5fdf095ef3d1a6cb84806504
Shane Loretz <shane.loretz@gmail.com>
2025-01-18 17:07:49 -0800

    Happy with rough layout
    Signed-off-by: Shane Loretz <shane.loretz@gmail.com>

--- src/treading/main.py
+++ src/treading/main.py
@@ -2,17 +2,36 @@ import kivy
 kivy.require('2.3.1')
 
 from kivy.app import App
-from kivy.uix.label import Label
+
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
+from kivy.core.window import Window
+Window.size = (500, 518)
+Window.always_on_top = True
+Window.minimum_width = 256
+# Window.borderless = True
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.widget import Widget
 
 
-class MyApp(App):
+class Issue(BoxLayout):
+    pass
+
+
+class IssueList(StackLayout):
+    pass
+
+
+class TreadingApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return IssueList()
 
 
 def main():
-    MyApp().run()
+    TreadingApp().run()
 
 
 if __name__ == '__main__':
